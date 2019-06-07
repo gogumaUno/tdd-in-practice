144dc351c1fd088098b43dd36320eadd3a82993f
vaninv <vaninv@dinamika.team>
2019-06-07 22:42:48 +0300

    Taxi-form

--- test.e2e.js
+++ test.e2e.js
@@ -1,18 +1,20 @@
 const timeout = 5000;
 
-describe(
-  '/ (Home Page)',
-  () => {
+describe('/ (Home Page)', () => {
     let page;
+
     beforeAll(async () => {
       page = await global.__BROWSER__.newPage();
       await page.goto('http://localhost:3000');
     }, timeout);
 
-    it('should load without error', async () => {
-      const text = await page.evaluate(() => document.body.textContent);
-      expect(text).toContain('Learn React');
+    it('(F01) Should provide quick form', async () => {
+      const form = await page.$eval('[data-marker="quick-form"]', f => f.innerText);
+      expect(form).toContain('Quick Form');
     });
-  },
-  timeout,
-);
+
+    it('(F02) Should provide full form', async () => {
+      const form = await page.$eval('[data-marker="full-form"]', f => f.innerText);
+      expect(form).toContain('Full Form');
+    });
+});
--- test/setup.js
+++ test/setup.js
@@ -8,7 +8,7 @@ const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 module.exports = async function() {
   const browser = await puppeteer.launch({
-    headless: false
+    headless: true
   });
   // store the browser instance so we can teardown it later
   // this global is only available in the teardown but not in TestEnvironments
